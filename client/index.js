451f9bae4cd43eef2a4878dbb31e2e83508f45a1
Jake Klingler <thearrow3456@gmail.com>
2016-10-13 18:22:08 -0600

    add basic voice commands with annyang

--- client/index.js
+++ client/index.js
@@ -1,18 +1,43 @@
 // @flow
 import { get, post } from './util'
+import annyang from 'annyang'
+
+const api = 'http://localhost:8081'
+
+function on() {
+  console.log('turning lights on')
+  post(`${api}/lights`, 'on')
+}
+
+function off() {
+  console.log('turning lights off')
+  post(`${api}/lights`, 'off')
+}
 
 (async() => {
-  const api = 'http://localhost:8081'
-
   const msg = await get(api)
   document.getElementById('connected').innerHTML = msg
-
-  document.getElementById('lights-on').addEventListener('click', (e) => {
-    post(`${api}/lights`, 'on')
-  })
-
-  document.getElementById('lights-off').addEventListener('click', (e) => {
-    post(`${api}/lights`, 'off')
-  })
-
 })()
+
+if (annyang) {
+  const commands = {
+    "lights on": () => on(),
+    "lights off": () => off(),
+  }
+  annyang.addCommands(commands)
+
+  annyang.addCallback('start', () =>
+    document.getElementById('listening').innerHTML = 'Annyang is Listening...'
+  )
+  annyang.addCallback('result', (result) =>
+    console.log(result)
+  )
+  annyang.addCallback('error', (error) =>
+    console.log(error)
+  )
+
+  annyang.start()
+}
+
+document.getElementById('lights-on').addEventListener('click', (e) => on())
+document.getElementById('lights-off').addEventListener('click', (e) => off())
